ca28cef0340a7b37b1a6a30d975fe45c9b0a0aba
mrhatimdarbali <mrhatimdarbali@gmail.com>
2023-12-07 07:30:59 -0800

    o

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
 
-env.hosts = ['54.210.52.84', '34.203.38.250']
+env.hosts = ['18.207.1.87', '52.206.189.175']
 
 
 @runs_once
@@ -48,3 +48,14 @@ def do_deploy(archive_path):
         return True
     except Exception:
         return False
+
+
+@task
+def deploy():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
